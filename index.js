a5fc3e575b90539e12cee030965a50e0e393b70c
Terrell Murrell <littlerell12@gmail.com>
2020-08-07 01:45:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts() {
   var array = [];
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var i = 0;
   while (array.length < facts.length) {
     array.push(facts[i] + "!!!");
@@ -25,4 +24,4 @@ function iLoveTheBeatles(number) {
     number += 1
   } while (number < 15);
   return array;
-} 
+}
